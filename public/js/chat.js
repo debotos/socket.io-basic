05e7893926397447c05313a972d7384acc330bb6
Debotos Das <debotosdas@gmail.com>
2019-03-16 11:07:44 +0600

    acknowledagement code added

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,37 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const message = e.target.elements.message.value;
+  const message = event.target.elements.message.value;
 
-  socket.emit('sendMessage', message);
-})
+  /*
+    'sendMessage' is the event name after that everything is
+    an argument of .on() receiver's callback function in serverside
+    Here, 'message' and 'the function' is passed as 1st & 2nd argument
+
+    so, in short, 2nd & 3rd argument of emit() will be 
+    the 1st & 2nd argument of on() function's callback 
+  */
+  socket.emit('sendMessage', message, (error) => {
+    if (error) { // stop execution via acknowledagement
+      return console.log(error);
+    }
+
+    console.log('Message delivered!');
+  });
+});
+
+document.querySelector('#share-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert("Your browser doesn't support Geolocation.");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, (message) => {
+      console.log(message);
+    })
+  });
+
+});
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express(); // 1. create express app
 const server = http.createServer(app); // 2. put express app inside httpServer
@@ -18,14 +19,56 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 /*
-  socket.emit(); // This emit event for particular client
-  io.emit();     // This emit event for all client
+  # 3 ways via the server can emit event
+  socket.emit();           // This emit event for current client(particular)
+  io.emit();               // This emit event for all client including current
+  socket.broadcast.emit()  // This emit event for all client except current one
+
+  # Built-in Events
+  io.on('connection')      // Used for new connection(Note that io.on(<built in event>))
+  socket.on('disconnect')  // Run whenever a client gets disconnected
+
+  # Event Acknowledgement
+  Via acknowledgement the client would get notified that the message(content) delivered successfully.
+  Simply, the client would get notified that the server processed the content that begin sent.
+  It can be reverse means server would get notified if the message delivered successfully to client.
+  It's just a matter of setting callback in server or client emited event.
+
+  server(emit) -> client(receive) --acknowledgement--> server
+  client(emit) -> server(receive) --acknowledgement--> Client
+
+  It provided via 3rd argument of emit() like,
+    emit(nameOfTheEvent, data, acknowledgementFunction)
+  It accessed via 2nd argunent of on() like,
+    on(nameOfTheEvent, (data, acknowledgementFunction) => { acknowledgementFunction() })
 */
 io.on('connection', (socket) => {
   socket.emit('message', 'Welcome!');
-  socket.on('sendMessage', (message) => {
+
+  socket.broadcast.emit('message', 'A new user has joined!');
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+
+    if (filter.isProfane(message)) {
+      // This is an example of 'acknowledgement '
+      // It is going to check the message and if it contains Profanity it will stop execution
+      // it is using return keyword. so nothing will execute after it.
+      return callback('Profanity is not allowed!');
+    }
+
     io.emit('message', message);
+    callback();
   })
+
+  socket.on('sendLocation', (coords, sendStatus) => {
+    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    sendStatus('Location shared!');
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
+  });
 })
 
 module.exports = {
